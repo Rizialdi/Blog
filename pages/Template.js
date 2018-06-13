a02a7c511be3fbd1ce8d66d03dee8d13c9eed003
rasselio-diack <rasselio.diack@gmail.com>
2018-06-13 06:00:44 +0200

    Created Template.js to be a template for handling posts/*.md file request

---
+++ pages/Template.js
@@ -0,0 +1,22 @@
+import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
+import Layout from '../components/Layout';
+
+const Template = (props) => {
+    return (
+    <Layout page_title = {`Posts/${props.source.data.title}`}>
+        <Markdown source = {props.source.data.body} />
+    </Layout>
+)}
+
+Template.getInitialProps = async function(context) {
+  const {identifiant } = context.query
+  const res = await fetch(`http://localhost:3000/api/${identifiant}`.toLowerCase())
+  const data = await res.json()
+
+  return {
+    source: data
+  }
+}
+
+export default Template
