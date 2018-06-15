234e19cb8062b20740c95e197f0828138c28f697
rasselio-diack <rasselio.diack@gmail.com>
2018-06-15 12:56:27 +0200

    Designed Title component

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 var PageTitle = (props) => {
     return (
-        <div>
-            <div className='pageTitle'>
-                <h1>{props.active_item}</h1>
-            </div>
+        <div className='pageTitle'>
+            <h1>{props.active_item}</h1>
             <style jsx>{`
                 .pageTitle {
                     background-color: #f3f3f3;
                     text-align: center;
-                    border-bottom: 0.3rem dashed black;
-                    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+                    border-bottom: 2px solid black;
+                    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                    margin-top: 0px
                 }
             `}</style>
         </div>
