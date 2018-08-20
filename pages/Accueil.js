5a0b121b2e662fadd91e293e49d17a65310c907a
rizialdi <rasselio.diack@gmail.com>
2018-08-20 19:16:30 +0200

    Updated Acceuil page content

--- pages/Accueil.js
+++ pages/Accueil.js
@@ -2,23 +2,31 @@ import Layout from '../components/Layout';
 
 export default () => {
     return (
-        <Layout page_title = "Accueil">
+        <Layout page_title="Accueil">
             <p>
-                Je suis un enfant de la rue, je dessine et passe du 
-                temps avec les enfants; Je possède une moto et je vis
-                au gré des vagues d'air -- King
-                <br />
+                Je suis titulaire d'un Master en <a href="http://master-spim.formation.univ-lorraine.fr/pcm-2/" target="_blank">Physique et Chimie des Matériaux </a>
+                à l'<a href="https://fr.wikipedia.org/wiki/Universit%C3%A9_de_Lorraine" target="_blank">Université de Lorraine</a>.
+                <br /><br />
+                L'objectif de ce blog est de presenter certains concept d'apprentissage automatique et leur utilité.
             </p>
             <style jsx>{`
                 * {
                     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
                     font-weight: 400;
                     font-size: 1.5rem;
+                    max-width: 1000px;
                 }
                 a {
-                    color: darkblue;
+                    color: gray;
+                }
+                strong {
+                    font-weight: 600;
+                    font-family: BlinkMacSystemFont
+                }
+                em {
+                    font-style: italic;
                 }
             `}</style>
         </Layout>
-)
+    )
 }
