be9359c7395b47337d3ae6d1b825dd5167b93fd8
rizialdi <rasselio.diack@gmail.com>
2019-04-17 20:17:13 +0200

    Polished search icon and filtering input box

--- components/Search.js
+++ components/Search.js
@@ -5,22 +5,33 @@ class Search extends Component {
         super(...args);
         this.state = {
             filterTerm: '',
+            search_click: false
+        };
+        this._handleChange = this._handleChange.bind(this);
     };
-      this._handleChange = this._handleChange.bind(this);
-};
 
-    _handleChange(e){
+    _handleChange(e) {
         const filterTerm = e.target.value;
-        this.setState({filterTerm})
+        this.setState({ filterTerm })
     }
-    
+
     render() {
         return (
             <div className='search_component'>
                 <div className="search">
-                    <input type = "text" name = "filter_search" value = {this.state.filterTerm} maxLength = "100" onChange={this._handleChange}/>
-                    <img src="../static/images/search.svg" alt="search_icon" height="42" width="42" /> 
-                </div>
+                    {this.state.search_click ? <input type="text"
+                        name="filter_search" value={this.state.filterTerm} placeholder="tag or sujects" 
+                        maxLength="10" onChange={this._handleChange} /> : ''}
+                    {this.state.search_click?
+                            <img src="../static/images/cancel.svg"
+                             alt="search_icon" className="close_icon"
+                              onClick={() => { this.setState({ search_click: !this.state.search_click }) }} /> :
+                            <img src="../static/images/search.svg"
+                             alt="close_icon" className="search_icon"
+                             onClick={() => { this.setState({ search_click: !this.state.search_click }) }} /> 
+                }
+                    
+                       </div>
                 <style jsx>{`
                     input {
                         width: 10em;
@@ -34,6 +45,7 @@ class Search extends Component {
                         border-bottom: solid black 0.1em;
                         background-color: #dadada;
                         border-radius: 0.5em 0.5em 0 0;
+                        transition-duration: 0.7s;
                     }
                     @keyframes blink { 
                         50% { border-bottom: solid #000000 0.1em; } 
@@ -42,6 +54,8 @@ class Search extends Component {
                         background-color: #dadada;
                         animation: blink 0.5s step-end infinite alternate;
                         border: solid #dadada;
+                        width: 15em;
+                        transition-duration: 0.7s;
                     }
     
                     img {
@@ -58,6 +72,19 @@ class Search extends Component {
                         right: 3em;
                         top: 2em;
 
+                    }
+                    .search_icon {
+                        height: 2em;
+                        width: auto
+                    }
+                    .close_icon {
+                        height: 1.4em;
+                        width: auto
+                    }
+                    .close_icon:hover {
+                        transform: rotate(360deg);
+                        transition-duration: 0.7s;
+                        width: auto
                     }
                 `}</style>
             </div>
